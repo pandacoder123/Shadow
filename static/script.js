9ed8a9b3be33fe5e5f47cac64592068d6411489b
panda <91168831+pandacoder123@users.noreply.github.com>
2024-05-13 16:27:51 -0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -9,43 +9,7 @@ function darkMode() {
     return brightness < 128;
   }
 
-  function aboutBlank() {
-  const url = location.href;
-  const width = window.innerWidth;
-  const height = window.innerHeight;
-
-  let inFrame;
-
-  try {
-    inFrame = window !== top;
-  } catch (e) {
-    inFrame = true;
-  }
-
-  if (!inFrame && !navigator.userAgent.includes("Firefox")) {
-    const popup = window.open("about:blank", name, `width=${width},height=${height}`);
-
-    if (!popup || popup.closed) {
-      alert("Allow popups and redirects to hide this from showing up in your history.");
-    } else {
-      const doc = popup.document;
-      const iframe = doc.createElement("iframe");
-      const style = iframe.style;
-      const link = doc.createElement("link");
-
-
-      iframe.src = url;
-      style.position = "fixed";
-      style.top = style.bottom = style.left = style.right = 0;
-      style.border = style.outline = "none";
-      style.width = style.height = "100%";
-
-      doc.head.appendChild(link);
-      doc.body.appendChild(iframe);
-      window.location.replace("https://classroom.google.com/");
-    }
-  }
-}
+  
 
   if (isColorDark(bgColor)) {
     console.log('website is in dark mode');
@@ -82,7 +46,43 @@ function myFunction() {
 
 
 
+function aboutBlank() {
+  const url = location.href;
+  const width = window.innerWidth;
+  const height = window.innerHeight;
 
+  let inFrame;
+
+  try {
+    inFrame = window !== top;
+  } catch (e) {
+    inFrame = true;
+  }
+
+  if (!inFrame && !navigator.userAgent.includes("Firefox")) {
+    const popup = window.open("about:blank", name, `width=${width},height=${height}`);
+
+    if (!popup || popup.closed) {
+      alert("Allow popups and redirects to hide this from showing up in your history.");
+    } else {
+      const doc = popup.document;
+      const iframe = doc.createElement("iframe");
+      const style = iframe.style;
+      const link = doc.createElement("link");
+
+
+      iframe.src = url;
+      style.position = "fixed";
+      style.top = style.bottom = style.left = style.right = 0;
+      style.border = style.outline = "none";
+      style.width = style.height = "100%";
+
+      doc.head.appendChild(link);
+      doc.body.appendChild(iframe);
+      window.location.replace("https://classroom.google.com/");
+    }
+  }
+}
 
 
 function byId(id) {
